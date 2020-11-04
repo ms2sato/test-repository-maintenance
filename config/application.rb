968fa18483c8c911f458359953b7c90649b2b816
ms2sato <ms2.sato@gmail.com>
2020-11-04 11:23:34 +0900

    branch-after-remote-big-images

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module TestRepositoryMaintenance
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # BRANCH-AFTER=REMOTE=BIG=IMAGES
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
